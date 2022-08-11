1b87c9f6177ce69a431eaf0646d40be851942bb9
mikhail.matskevich <mih198830@gmail.com>
2022-08-11 10:42:21 +0400

    deleted not working test

--- src/test/java/tests/AndroidSearchTests.java
+++ src/test/java/tests/AndroidSearchTests.java
@@ -10,18 +10,6 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class AndroidSearchTests extends TestBase {
-    @Test
-    @Disabled
-    void searchTest() {
-        step("Type search", () -> {
-            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text"))
-                    .sendKeys("BrowserStack");
-        });
-        step("Verify content found", () ->
-                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .shouldHave(sizeGreaterThan(0)));
-    }
 
     @Test
     void settingMenuAvailable() {
